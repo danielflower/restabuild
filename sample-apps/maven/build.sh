766ba9fe1b431323035854043d1e50501c1b2482
Daniel Flower <git@danielflower.com>
2021-08-29 21:28:22 +0800

    Fixing build.sh for test

--- sample-apps/maven/build.sh
+++ sample-apps/maven/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-echo "build parameter 1: --%1--"
-echo "build parameter 2: --%2--"
+echo "build parameter 1: --$1--"
+echo "build parameter 2: --$2--"
 mvn --version
 mvn releaser:release
